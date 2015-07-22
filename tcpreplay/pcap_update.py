de44a4d8f1d6dc7eafbcdcc853dd4447d7f4efdd
zhuyl <yongliang095@qq.com>
2015-07-22 14:40:36 -0400

    remove print

--- tcpreplay/pcap_update.py
+++ tcpreplay/pcap_update.py
@@ -14,7 +14,6 @@ def get_ftp_file(filePath):
     f.connect('192.168.2.105',21,10)
     f.login('anonymous','')
     f.cwd('/replay_pcap')
-    print "filePath:",filePath
     filename='RETR '+filePath
     f.retrbinary(filename,open(filePath,'wb').write)
     f.quit()
@@ -27,18 +26,14 @@ def update_pcaps():
     l=open(pcap_num,'r')
     l_num=l.readlines()
     l.close()
-    print "r_num",r_num
-    print "l_num",l_num
     if int(r_num[0]) != int(l_num[0]) :
         f=open(pcaps_list,'r')
         lpcap_path=f.readlines()
         f.close()
-        print "lpcap_path:",lpcap_path
         get_ftp_file(pcaps_list)
         f=open(pcaps_list,'r')
         rpcap_path=f.readlines()
         f.close()
-        print "rpcap_path:",rpcap_path
         for i in range(len(rpcap_path)):
             rpcap_path[i]=rpcap_path[i].replace('\n','')
             for j in range(len(lpcap_path)):
@@ -49,7 +44,7 @@ def update_pcaps():
                 else:
                     have_pcap=1
             if int(have_pcap)==int(1):
-                print "get pcap:",rpcap_path[i]
+                print "update pcap:",rpcap_path[i]
                 get_ftp_file(rpcap_path[i])
                 
 update_pcaps()
