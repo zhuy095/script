b9b86482e772c91adf22b919892c64054e198f63
yongliang095@qq.com <yongliang095@qq.com>
2016-12-19 15:26:45 +0800

    添加收发解析报文的脚本

---
+++ udp_send/mul_sendpack.py
@@ -0,0 +1,141 @@
+﻿#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+# UDP Client - udpclient.py
+# code by zhuyl
+import socket, sys,time,threading,random,select,struct
+import pdb
+
+def ip_change(startip='',count=None):
+    ip=[startip]
+    ip_old=startip.split('.')
+    for i in range(count-1):
+        ip_old[3]=int(ip_old[3])+1
+        if int(ip_old[3]) == 255 :
+            ip_old[3]=1
+            ip_old[2]=int(ip_old[2])+1
+            if int(ip_old[2]) == 255 :
+                ip_old[2] = 1
+                ip_old[1] = int(ip_old[1])+1
+                if int(ip_old[1])==255:
+                    ip_old[1] = 1
+                    ip_old[0] = int(ip_old[0])+1
+        for a in range(len(ip_old)):
+            ip_old[a]=str(ip_old[a])
+        ip.append('.'.join(ip_old))
+    return ip
+
+class vfw_simulation():  #vfw 实例
+    status=0
+    sessionID=0
+    runTime=time.time()
+    smcRunTime=time.time()
+    def __init__(self,ip):
+        self.ip=ip
+        self.sn='10076-0023I-2000T-E404P-'+str(10000+int(ip.split('.')[-1]))
+        
+    def _pack_header(self):
+        sequence=2
+        reserved=0
+        version=2
+        timestamp=time.time()
+        header=struct.pack('!HbbHHif',version,self.command[0],self.command[1],sequence,reserved,self.sessionID,timestamp)
+        return header
+    
+    def unpack_header(self,data):
+        command=[0,0]
+        if len(data) == 16 :
+            ver,command[0],command[1],sequence,reserved,sessionID,timestamp=struct.unpack('!HbbHHif',data)
+        else :
+            ver,command[0],command[1],sequence,reserved,sessionID,timestamp,cloud_data=struct.unpack('!HbbHHif%ds'%(len(data)-16),data)
+        if  command == [1,2] :  # 绑定应答报文
+            self.sessionID=sessionID
+            self.status = 1 #云端给绑定做应答，下次发送请求license
+        elif command == [6,2] :  # update 
+            self.license = cloud_data.decode('utf-8')
+            self.status = 2  #license 更新后，只发送keepalive
+        elif command == [6,3]: #收到云端发送的报活keepalive
+            self.smcRunTime=time.time()  #收到smc的应答报文，更新smc在线时间。
+            #pass
+        return self.send_pack()
+            
+    def _send_bindcode(self):
+        self.command=[1,1]
+        data='{"sn": "'+self.sn+'", "bindCode": "aed995e7ce694c4287d9cd512e3d3229"}'
+        return self._pack_header()+data.encode('ascii')
+        
+    def _send_licence_request(self):
+        self.command=[6,1]
+        self.core='{"core": "4"}'
+        return self._pack_header()+self.core.encode('ascii')
+    def _send_device_notice(self):  #agent keepalive
+        self.command=[2,1]
+        if self.status == 2 :
+            dev_info='{"hostName": "'+self.ip+'", "hostIP": "'+self.ip+'", "version": "V1.1-R2.120161212", "uptime": "'+str(int(time.time()-self.runTime))+'", "cpuInfo": {"total": "100.00", "used": "0.00"}, "memInfo": {"total": "1096636116", "used": "455400072"}, "nrConns": "30", "nrUsers": "52", "cycle": "10", "license": '+self.license+'}'
+        else :
+            dev_info='{"hostName": "'+self.ip+'", "hostIP": "'+self.ip+'", "version": "V1.1-R2.120161212", "uptime": "'+str(int(time.time()-self.runTime))+'", "cpuInfo": {"total": "100.00", "used": "0.00"}, "memInfo": {"total": "1096636116", "used": "455400072"}, "nrConns": "30", "nrUsers": "52", "cycle": "10"}'
+        return self._pack_header()+dev_info.encode('ascii')
+        
+    def send_pack(self):
+        if int(time.time()-self.smcRunTime) > 300 :  #smc长时间不发送keepalive报文，则认为smc不在线，重新请求绑定。
+            self.status=0
+        if self.status == 0 : # 请求绑定
+            return self._send_bindcode()
+        elif self.status == 1:  # 请求license
+            return self._send_licence_request(),self._send_device_notice()
+        elif self.status == 2: # 只发送keepalive
+            return self._send_device_notice()
+        
+def sendPackSocket(local_ip,port=2000):
+    s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((local_ip,port))
+    s.setblocking(0)
+    return s
+
+def vfwScoket(ip_s):
+    vfw_objs={}
+    for i in range(len(ip_s)):
+        sendSocket=sendPackSocket(ip_s[i])  #根据本地IP生成sock
+        vfw=vfw_simulation(ip_s[i])  #根据IP生成vfw实例
+        vfw_objs[sendSocket]=vfw   #把socket和vfw实例进行关联
+    return vfw_objs
+        
+def run_main(vfw_objs):
+    loops=0
+    while True :
+        rs,ws,es=select.select(vfw_objs.keys(),[],[],5) #,timeout=5
+        if rs == []:  ## 如果超时，则发送保活报文，每5s发送一次。
+            for sock in vfw_objs.keys():
+                pack=vfw_objs[sock].send_pack()
+                if type(pack) == tuple :  # status==2， 需要发送两个报文。
+                    for i in range(len(pack)):
+                        sock.sendto(pack[i],dest)
+                else :
+                    sock.sendto(pack,dest)
+        else:  #vfw_objs.keys()被触发，有读操作(rs有返回值)。
+            for sock in rs:   ##如果收到udp报文，则进行处理，并发送应答
+                if sock in vfw_objs.keys():
+                    message, address = sock.recvfrom(8192)
+                    pack=vfw_objs[sock].unpack_header(message)
+                    if type(pack) == tuple :
+                        for i in range(len(pack)):
+                            sock.sendto(pack[i],dest)
+                    else :
+                        sock.sendto(pack,dest)
+        loops+=1
+        print("loop %d"%loops)
+        
+if __name__ == '__main__' :
+    #配置smc服务器IP
+    dest_ip='192.168.1.221'
+    port = 9070
+    dest=(dest_ip,port)
+
+    #配置vfw使用的本地
+    local_ip='172.17.22.1'
+    ip_num=100
+    ip_list=ip_change(local_ip,ip_num)
+    
+    pdb.set_trace()
+    vfw_objs=vfwScoket(ip_list)  #生成vfw和socket的对应关系
+    run_main(vfw_objs)
---
+++ udp_send/udp_small_frag_multiprocessing.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+import sys,time,re,subprocess,os,multiprocessing,uuid,signal
+from scapy.all import *
+
+conf='udp.conf'
+def get_file_value(conf):
+    new_conf=[]
+    f=open(conf,'r')
+    conf=f.readlines()
+    f.close()
+    for i in range(len(conf)):
+        if None!=re.match('.*=.*',conf[i]):
+            new_conf.append(conf[i])
+    return new_conf
+
+def find_value(key,new_conf):
+    for i in range(len(new_conf)):
+        p=re.compile(key)
+        if None!=p.match(new_conf[i]):
+            a=new_conf[i].split('=')
+    return a[1].replace('\n','')
+
+def get_ip(ip,num):
+    ip_new_list=[]
+    ip_new_list.append(ip)
+    ip_n=ip.split('.')
+    for i in range(int(num)-1):
+        tmp=ip_n[3]=int(ip_n[3])+1
+        if tmp  > 254 :
+            ip_n[3]=tmp%254
+            ip_n[2]=int(ip_n[2])+int(tmp)/254
+            tmp=ip_n[2]
+            if tmp > 254 :
+                ip_n[2]=int(tmp)%254
+                ip_n[1]=int(ip_n[1])+int(tmp)/254
+                tmp=ip_n[1]
+                if tmp > 254 :
+                   ip_n[1]=int(tmp)%254
+                   ip_n[0]=int(ip_n[0])+int(tmp)/254
+        ip_new_list.append(str(ip_n[0])+"."+str(ip_n[1])+"."+str(ip_n[2])+"."+str(ip_n[3]))
+    return ip_new_list
+
+def get_port(port,num):
+    ports=[]
+    ports.append(port)
+    for i in range(1,int(num),1):
+        ports.append(int(port)+i)
+    return ports
+
+def get_send_data(length):
+    data=''
+    for i in  range(int(length)-42) :
+        data+="a"
+    return data
+
+
+def get_mac_address(eth):
+    cmd="ifconfig "+eth+"  | grep HWaddr | awk \'{print $5}\'"
+    pipe=subprocess.Popen(cmd,stdin = subprocess.PIPE,stdout = subprocess.PIPE, stderr = subprocess.PIPE,shell=True)
+    pipe.wait()
+    return pipe.stdout.read().replace('\n','')
+    pipe.kill()
+
+def get_send_value():
+    confs=get_file_value(conf)
+    src_eth=find_value("src_eth",confs)
+    src_ip=find_value("src_ip",confs)
+    srcip_num=find_value("srcip_num",confs)
+    src_port=find_value("src_port",confs)
+    srcport_num=find_value("srcport_num",confs)
+    src_dmac=find_value("src_dmac",confs)
+    dest_eth=find_value("dest_eth",confs)
+    dest_ip=find_value("dest_ip",confs)
+    destip_num=find_value("destip_num",confs)
+    dest_port=find_value("dest_port",confs)
+    destport_num=find_value("destport_num",confs)
+    dest_dmac=find_value("dest_dmac",confs)
+    mtu=find_value("mtu",confs)
+    run_time=find_value("run_time",confs)
+    load=get_send_data(mtu)
+    srcips=get_ip(src_ip,srcip_num)
+    destips=get_ip(dest_ip,destip_num)
+    srcports=get_port(src_port,srcport_num)
+    destports=get_port(dest_port,destport_num)
+    return srcips,destips,srcports,destports,src_dmac,dest_dmac,src_eth,dest_eth,load,run_time
+
+
+def get_send_pcaps(srcips,destips,srcports,destports,smac,dmac,load):
+    print("\nprepare pcaps,please waiting...")
+    print("srcip:%s"%srcips)
+    print("destip:%s"%destips)
+    print("srcport:%s"%srcports)
+    print("destport:%s"%destports)
+    pcaps=[]
+    Flags=0
+    Frag=0
+    t=0
+    load_l=len(load)
+    for srcip in srcips:
+        for destip in destips:
+            for srcport in srcports:
+                for destport in destports:
+                    srcport=int(srcport)
+                    destport=int(destport)
+                    pcaps.append(str(Ether(dst=dmac,src=smac)/IP(src=srcip,dst=destip,flags=Flags,frag=Frag)/UDP(sport=srcport,dport=destport,)/load))
+                    t+=1
+                    Flags=2
+                    Frag+=load
+                    if t >= 20 :
+                        t=0
+                        Flags=0
+                        Frag=0
+    print("prepare pcaps complete!")
+    return pcaps
+
+
+def get_socket(eth):
+    s = socket.socket(socket.AF_PACKET,socket.SOCK_RAW,0x0800)
+    s.bind((eth,50007))
+    return s
+
+def send_socket(sfile,pcaps,tims):
+    if tims == 0:
+        while True:
+            for i in range(len(pcaps)):
+                sfile.send(pcaps[i])
+    else:
+        t=0
+        while t<tims:
+            for i in range(len(pcaps)):
+                sfile.send(pcaps[i])
+            t+=1
+ 
+def get_arp_pcaps(ips,mac):
+    arppcaps=[]
+    for ip in ips:
+        arp=Ether(dst="ff:ff:ff:ff:ff:ff",src=mac,type=0x0806)/ARP(op=1,hwsrc=mac,psrc=ip,hwdst="00:00:00:00:00:00",pdst=ip)
+        arppcaps.append(str(arp))
+    return arppcaps
+
+def send_arp(sfile,pcaps):
+    for i in range(len(pcaps)):
+        sfile.send(pcaps[i])
+    time.sleep(30)
+    send_arp(sfile,pcaps)
+
+def handler(signum,frame):
+    for i in pro:
+        i.terminate()
+    s_file.close()
+    d_file.close()
+    cmd='echo 1 > /proc/sys/vm/drop_caches'
+    cleanmem_pipe=subprocess.Popen(cmd,stdin = subprocess.PIPE,stdout = subprocess.PIPE, stderr = subprocess.PIPE,shell=True)
+    cleanmem_pipe.wait()
+    time.sleep(1)
+    print("\nstop all......")
+
+
+
+try:
+    global pro,s_file,d_file
+    pro=[]
+    signal.signal(signal.SIGINT, handler)
+#    signal.signal(signal.SIGHUP, handler)
+#    signal.signal(signal.SIGTERM, handler)
+    srcips,destips,srcports,destports,src_dmac,dest_dmac,src_eth,dest_eth,load,run_time=get_send_value()
+    
+    src_smac=get_mac_address(src_eth)
+    src_send_pcaps=get_send_pcaps(srcips,destips,srcports,destports,src_smac,src_dmac,load)
+    src_arp_pcaps=get_arp_pcaps(srcips,src_smac)
+    
+    dest_smac=get_mac_address(dest_eth)
+    dest_send_pcaps=get_send_pcaps(destips,srcips,destports,srcports,dest_smac,dest_dmac,load)
+    dest_arp_pcaps=get_arp_pcaps(destips,dest_smac)
+
+    s_file=get_socket(src_eth)
+    d_file=get_socket(dest_eth)
+    
+    
+    t=multiprocessing.Process(target=send_arp,args=(s_file,src_arp_pcaps,))
+    pro.append(t)
+    t=multiprocessing.Process(target=send_arp,args=(d_file,dest_arp_pcaps,))
+    pro.append(t)
+    t=multiprocessing.Process(target=send_socket,args=(s_file,src_send_pcaps,int(run_time),))
+    pro.append(t)
+    t=multiprocessing.Process(target=send_socket,args=(d_file,dest_send_pcaps,int(run_time),))
+    pro.append(t)
+    
+    for t in pro:
+        t.daemon=False
+        t.start()
+        #t.join()
+    
+    print("\nEnter Ctrl+c to stop...")
+    
+except:
+    #handler()
+    print("have error,exit")
